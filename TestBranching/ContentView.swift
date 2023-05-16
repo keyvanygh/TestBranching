079ca674224dd9fdb08110012b5e4572ed87149f
Keyvan Yaghoubian <kyghoubian@moneygram.com>
2023-05-15 20:49:47 -0400

    commit 3

--- TestBranching/ContentView.swift
+++ TestBranching/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Commit 2")
+            Text("Commit 3")
         }
         .padding()
     }
