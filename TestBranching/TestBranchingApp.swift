254003f5d9ba7da5a4e00f6f2870233e30bf5b9d
Keyvan Yaghoubian <kyghoubian@moneygram.com>
2023-05-15 20:46:32 -0400

    Project Created

---
+++ TestBranching/TestBranchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBranchingApp.swift
+//  TestBranching
+//
+//  Created by Keyvan Yaghoubian on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TestBranchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
